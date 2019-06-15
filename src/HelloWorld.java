547e79223e39d2e97e5c6d8963a354bc59e341ab
Maks <petrenkomaks405@gmail.com>
2019-06-15 18:06:09 +0300

    HelooWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String []args){
+        Scanner s =new Scanner (System.in);
+        String string = s.nextLine();
+        System.out.println(string);
+    }
+}
